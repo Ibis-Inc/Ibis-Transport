04aeb2b43eca48be352abbbb3ec8d533688713ad
AppleSheep <95598768+appleshoops@users.noreply.github.com>
2024-08-13 21:36:56 +1000

    le epic commit

--- Ibis Transport/ContentView.swift
+++ Ibis Transport/ContentView.swift
@@ -4,22 +4,36 @@ import MapKit
 
 struct ContentView: View {
     
-    @State var showHomeSheet = false
+    @State var showHomeSheet = true
     
     var body: some View {
         ZStack {
             Map() {
             }
             .mapStyle(.standard(elevation: .realistic))
-            
-            Button("Show Home Sheet") {
-                showHomeSheet.toggle()
-            }
-            .buttonStyle(.bordered)
         }
         .sheet(isPresented: $showHomeSheet) {
-            Text("This is le home screen!!!")
+            homeSheetView()
+                .presentationDetents([.fraction(0.20), .large])
+                .presentationDragIndicator(.visible)
+                .presentationCornerRadius(50)
+                .presentationBackground(.ultraThinMaterial)
         }
+    }
+}
+
+struct homeSheetView: View {
+    var body: some View {
+            VStack (alignment: .leading) {
+                Text("Ibis Transport")
+                    .fontWeight(.bold)
+                    .fontWidth(.expanded)
+                    .font(.system(size: 30))
+                Spacer()
+                    .frame(height: 60)
+            }
+            .padding()
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
     }
 }
 
